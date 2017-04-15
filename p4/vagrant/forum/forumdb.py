4dc38efa2d6269a7d2c8731b2770375859e2a452
alisaleemh <ali.s1995@gmail.com>
2017-04-15 13:46:24 -0400

    postgresdb working for p4

--- p4/vagrant/forum/forumdb.py
+++ p4/vagrant/forum/forumdb.py
@@ -1,31 +1,68 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
 import time
+import psycopg2
 
-## Database connection
-DB = []
+# Dict to store db specific parameters
+def ConnectDb():
+    db_params = {
+                'dbname': 'forum',
+                'dbuser': 'alisaleemh'
+                }
+    # Database connection
+    try:
+        db = psycopg2.connect("dbname=%s" % db_params['dbname'])
+        print 'Connected to %s' % db_params['dbname']
+        return db
+    except:
+        print "Error, could not connect to the database"
+        return
 
-## Get posts from database.
+
+# Get posts from database.
 def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
+    """Get all the posts from the database, sorted with the newest first.
 
     Returns:
       A list of dictionaries, where each dictionary has a 'content' key
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
-    '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
-    return posts
+    """
+    db = ConnectDb()
+    cur = db.cursor()
+    cur.execute("SELECT content,time FROM POSTS ORDER BY TIME ASC")
 
-## Add a post to the database.
+    posts = ({
+            'content': str(row[0]),
+            'time': str(row[1])
+            } for row in cur.fetchall())
+    db.close()
+    if posts:
+        return posts
+    else:
+        return
+
+
+# Add a post to the database.
 def AddPost(content):
     '''Add a new post to the database.
 
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    db = ConnectDb()
+    cur = db.cursor()
+
+    try:
+        cur.execute("INSERT INTO POSTS  (content) VALUES ('%s')" % (content,))
+    except:
+        print "Failed to insert"
+
+    try:
+        db.commit()
+    except:
+        print "Failed to commit"
+        db.rollback()
+    db.close()
